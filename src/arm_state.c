d5b43816c016e9570c0fb5943ec64c2c87ed875f
TaoWang <tw520@imperial.ac.uk>
2021-05-23 13:21:22 +0100

    moved definitions and struct to header file

--- src/arm_state.c
+++ src/arm_state.c
@@ -1,41 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "arm_state.h"
 
-#define MEM_CAPACITY 65536
-#define NUM_REGISTERS 17
-
-//data structure to capture the state of the ARM machine
-typedef struct {
-    int memory[MEM_CAPACITY];
-    int registers[NUM_REGISTERS];
-} ARM_STATE;
-
-void initialise(ARM_STATE state);
-void terminate(ARM_STATE state);
+void initialise(ARM_STATE *state);
+void terminate(ARM_STATE *state);
 
 //sets value of all registers to 0 
 //sets value of all memory locations to 0
-void initialise(ARM_STATE state) {
+void initialise(ARM_STATE *state) {
     for (int i = 0; i < NUM_REGISTERS; i++) {
-        state.registers[i] = 0;
+        state->registers[i] = 0;
     }
 
     for (int i = 0; i < MEM_CAPACITY; i++) {
-        state.memory[i] = 0;
+        state->memory[i] = 0;
     }
 }
 
 /*if an all-0 instruction is met then the emulator will terminate, 
 this function will be called which prints out the contents of the registers and non-zero memory locations
 */
-void terminate(ARM_STATE state) {
+void terminate(ARM_STATE *state) {
+    printf("Registers:\n");
     for (int i = 0; i < NUM_REGISTERS; i++) {
-        printf("$%d: %x\n", i, state.registers[i]);
+        printf("$%d: %x\n", i, state->registers[i]);
     }
-
+    printf("Non-zero memory:\n");
     for (int i = 0; i < MEM_CAPACITY; i++) {
-        if (state.memory[i] != 0) {
-            printf("0x%x: %x", i * 4, state.memory[i]);
+        if (state->memory[i] != 0) {
+            printf("0x%x: %x\n", i * WORD_SIZE, state->memory[i]);
         }
     }
+    printf("Emulator terminated.\n");
 }
--- src/emulate.c
+++ src/emulate.c
@@ -1,9 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binreader.h"
+#include "arm_state.h"
 
 int main(int argc, char **argv) {
+
+  ARM_STATE ARM_machine;
+  ARM_STATE *machinePtr = &ARM_machine;
+
+  initialise(machinePtr);
+
   readBin(argv[1]); // reads the file input
+
+  //fetch decode execute loop here until termination
+
+  terminate(machinePtr);
+  
   return 0;
 }
 
