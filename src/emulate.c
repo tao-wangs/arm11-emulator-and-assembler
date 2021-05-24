47e3c555889717d1d0995b4a26d189181e875a4d
la320 <la320@imperial.ac.uk>
2021-05-24 13:07:26 +0100

    changed stuff

--- src/emulate.c
+++ src/emulate.c
@@ -3,7 +3,13 @@
 #include "binreader.h"
 
 int main(int argc, char **argv) {
-  readBin(argv[1]); // reads the file input
+  unsigned int *prog = readBin(argv[1]); // reads the file input
+  printf("%x\n", prog[1]);
+  char* bin = binRep(prog[1]);
+  for(int i = 0; i<32; i++){
+    printf("%c", bin[i]);
+  }
+  printf("\n");
   return 0;
 }
 
