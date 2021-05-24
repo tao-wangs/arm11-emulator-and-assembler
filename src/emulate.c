b8b95f3eb9c60cf0139a32119f65399bb60cad2e
la320 <la320@imperial.ac.uk>
2021-05-24 13:28:32 +0100

    removed test from main

--- src/emulate.c
+++ src/emulate.c
@@ -5,19 +5,11 @@
 
 int main(int argc, char **argv) {
   unsigned int *prog = readBin(argv[1]); // reads the file input
-  printf("%x\n", prog[1]);
-  char* bin = binRep(prog[1]);
-  for(int i = 0; i<32; i++){
-    printf("%c", bin[i]);
-  }
-  printf("\n");
-
+  
   ARM_STATE ARM_machine;
   ARM_STATE *machinePtr = &ARM_machine;
 
   initialise(machinePtr);
-
-  readBin(argv[1]); // reads the file input
 
   //fetch decode execute loop here until termination
 
