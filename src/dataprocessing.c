9e076d6596585e6d3bccc394db06b824c5e2067b
Armaan <as3820@imperial.ac.uk>
2021-05-25 08:05:54 -0700

    Attempting to add the functions to determine what type of operation is given

--- src/dataprocessing.c
+++ src/dataprocessing.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+#define AND "0000"
+#define EOR "0001"
+#define SUB "0010"
+#define RSB "0011"
+#define ADD "0100"
+#define TST "1000"
+#define TEQ "1001"
+#define CMP "1010"
+#define ORR "1100"
+#define MOV "1101"
+
 void dataProcessingInstruction(char *instruction) {
 	char cond[4];
 	char immediateOperand;
@@ -37,10 +48,20 @@ int conditionCodeIsSet(char setConditionCode) {
 	return setConditionCode == '1';
 }	
 
+// See whether the opcode string is equal to that off any of the arithmetic operations, and return 1 if it matches any of them.
 int operationIsArithmetic(char *opcode) {
-
+	return ((strcmp(opcode, ADD) == 0) || 
+		(strcmp(opcode, SUB) == 0) || 
+		(strcmp(opcode, RSB) == 0) || 
+		(strcmp(opcode, CMP) == 0))
 }
 
+// See whether the opcode string is equal to that off any of the logical operations, and return 1 if it matches any of them.
 int operationIsLogic(char *opcode) {
-
+	return ((strcmp(opcode, AND) == 0) || 
+		(strcmp(opcode, EOR) == 0) || 
+		(strcmp(opcode, ORR) == 0) || 
+		(strcmp(opcode, TEQ) == 0) || 
+		(strcmp(opcode, TST) == 0) || 
+		(strcmp(opcode, CMP) == 0));
 }
