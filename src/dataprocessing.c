d1bbab3dac51003a475f0072b7b4794c7b00e258
aah120 <aah120@ic.ac.uk>
2021-05-25 20:28:52 +0100

    Fix functions in dataprocessing.c and using different method to create the arrays

--- src/dataprocessing.c
+++ src/dataprocessing.c
@@ -1,56 +1,95 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "dataprocessing.h"
 
 void dataProcessingInstruction(char *instruction) {
-	char cond[4];
-	char immediateOperand;
-	char opcode[4];
-	char setConditionCode;
-	char rn[4];
-	char rd[4];
-	char operand2[12];
+
+	char *cond = malloc(4);
+	char *immediateOperand = malloc(1);
+	char *opcode = malloc(4);
+	char *setConditionCode = malloc(1);
+	char *rn = malloc(4);
+	char *rd = malloc(4);
+	char *operand2 = malloc(12);
 	
 	strncpy(cond, instruction, 4);
-	cond[4] = '\0';
 	
 	strncpy(immediateOperand, instruction+6, 1);
 	strncpy(opcode, instruction+7, 4);
 	
 	strncpy(setConditionCode, instruction+11, 1);
 	strncpy(rn, instruction+12, 4);
-	rn[4] = '\0';
 	
 	strncpy(rd, instruction+16, 4);
-	rd[4] = '\0';
 	
 	strncpy(operand2, instruction+20, 12);
-	operand2[12] = '\0';
 }
 
 // Checks whether the immediate operand bit is set to 1 or not.
-int immediateOperandBitIsSet(char immediateOperand) {
-	return immediateOperand == '1';
+int immediateOperandBitIsSet(char *immediateOperand) {
+	return strcmp(immediateOperand, "1") == 0;
 }
 
 // Check whether the flags condition code is set.
-int conditionCodeIsSet(char setConditionCode) {
-	return setConditionCode == '1';
+int conditionCodeIsSet(char *setConditionCode) {
+	return strcmp(setConditionCode, "1") == 0;
 }	
 
 int operationIsArithmetic(char *opcode) {
-	return !(strcmp(opcode, sub) || 
-			strcmp(opcode, rsb) || 
-			strcmp(opcode, add) || 
+	return !(strcmp(opcode, sub) && 
+			strcmp(opcode, rsb) && 
+			strcmp(opcode, add) && 
 			strcmp(opcode, cmp));
 }
 
 int operationIsLogic(char *opcode) {
-	return !(strcmp(opcode, and) ||
-			strcmp(opcode, eor) ||
-			strcmp(opcode, orr) ||
-			strcmp(opcode, teq) ||
-			strcmp(opcode, tst) ||
+	return !(strcmp(opcode, and) &&
+			strcmp(opcode, eor) &&
+			strcmp(opcode, orr) &&
+			strcmp(opcode, teq) &&
+			strcmp(opcode, tst) &&
 			strcmp(opcode, mov));
+}
+
+int main(void) {
+
+	char *instruction = "11110000100100000000000000000000";
+
+	char *cond = malloc(4);
+	char *immediateOperand = malloc(1);
+	char *opcode = malloc(4);
+	char *setConditionCode = malloc(1);
+	char *rn = malloc(4);
+	char *rd = malloc(4);
+	char *operand2 = malloc(12);
+	
+	strncpy(cond, instruction, 4);
+	printf("cond: %s\n", cond);
+
+	strncpy(immediateOperand, instruction+6, 1);
+	printf("imm: %s\n", immediateOperand);
+	strncpy(opcode, instruction+7, 4);
+	printf("opcode: %s\n", opcode);
+	
+	strncpy(setConditionCode, instruction+11, 1);
+	printf("setcond: %s\n", setConditionCode);
+	strncpy(rn, instruction+12, 4);
+	printf("rn: %s\n", rn);
+	
+	strncpy(rd, instruction+16, 4);
+	printf("rd: %s\n", rd);
+	
+	strncpy(operand2, instruction+20, 12);
+	printf("op2: %s\n", operand2);	
+
+	printf("\n");
+
+	printf("immset? %i\n", immediateOperandBitIsSet(immediateOperand));
+	printf("condset? %i\n", conditionCodeIsSet(setConditionCode));
+	printf("arith? %i\n", operationIsArithmetic(opcode));
+	printf("logic? %i\n", operationIsLogic(opcode));
+
+	return 0;
 }
