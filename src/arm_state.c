f5d11d14cf5ee677d4bcd484794c5c87ee5cae2d
aah120 <aah120@ic.ac.uk>
2021-05-25 12:16:11 +0100

    Altered terminate to show the values of non general purpose registers labelled by their name i.e. PC instead of 5

--- src/arm_state.c
+++ src/arm_state.c
@@ -23,7 +23,23 @@ this function will be called which prints out the contents of the registers and 
 void terminate(ARM_STATE *state) {
     printf("Registers:\n");
     for (int i = 0; i < NUM_REGISTERS; i++) {
-        printf("$%d: %x\n", i, state->registers[i]);
+        switch (i)
+        {
+            case 13:
+                printf("SP: %x\n", state->registers[SP]);
+                break;
+            case 14:
+                printf("LR: %x\n", state->registers[LR]);
+                break;
+            case 15:
+                printf("PC: %x\n", state->registers[PC]);
+                break;
+            case 16:
+                printf("CPSR: %x\n", state->registers[CPSR]);
+                break;
+            default:
+                printf("$%d: %x\n", i, state->registers[i]);
+        } 
     }
     printf("Non-zero memory:\n");
     for (int i = 0; i < MEM_CAPACITY; i++) {
