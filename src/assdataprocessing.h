907af341c451165bbe0d6e250ffcdbd9039a308e
aah120 <aah120@ic.ac.uk>
2021-06-11 11:39:24 +0100

    refactor: remove magic numbers for assdataprocessing.c and replace with defined constants from header file

--- src/assdataprocessing.h
+++ src/assdataprocessing.h
@@ -1,9 +1,15 @@
-#ifndef __ASSEMBLEDATAPROCESSING__
-#define __ASSEMBLEDATAPROCESSING__
+#ifndef ASSEMBLEDATAPROCESSING_H
+#define ASSEMBLEDATAPROCESSING_H
 
 #define ONE_BYTE_MAX_INT 255
 #define ONE_NIBBLE_MAX_INT 15
 
+#define FILLER_SHIFT 26
+#define RN_SHIFT 16
+#define RD_SHIFT 12
+#define ROTATE_AMT_SHIFT 8 
+
+int32_t assembleDataProcessing(char *mnemonic, char *dstreg, char *srcreg, char *op2, hashTable *table);
 int32_t generate8BitImmediate(char *operand2);
 int32_t undoRotation(int32_t immOperand);
 
