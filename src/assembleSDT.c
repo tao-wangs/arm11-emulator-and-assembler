3cd375a0844eb6d4916bf0d72d89ee7c71dfa6a2
TaoWang <tw520@imperial.ac.uk>
2021-06-11 14:33:14 +0100

    fix<assembleSDT.c>: fixed control flow and removed duplicate code

--- src/assembleSDT.c
+++ src/assembleSDT.c
@@ -18,7 +18,7 @@ int32_t assembleLDR(char* mnemonic, char* op1, char* op2) {
     int32_t l = 1 << 20;
     int32_t rn;
     int32_t rd = stringToInt(op1) << 12;
-    int32_t offset;
+    int32_t offset = 0;
 
     if (!strcmp(op2[0], "=")) {
         if (stringToInt(op2) <= 0xFF) {
@@ -31,7 +31,6 @@ int32_t assembleLDR(char* mnemonic, char* op1, char* op2) {
         }
     } else if (strlen(op2) <= 5) {
         rn = stringToInt(removeBrackets(op2));
-        offset = 0;
     } else {
         int32_t index = 0;
         for (int32_t i = 0; i < strlen(op2); i++) {
@@ -39,14 +38,13 @@ int32_t assembleLDR(char* mnemonic, char* op1, char* op2) {
                break;
            }
         }
+        char* endPtr;
         if ((!index == 3) && !(index == 4)) {
-            char* endPtr;
-            rn = stringToInt(strtok_r(removeBrackets(op2), ",", &endPtr));
+            rn = stringToInt(strtok_r(removeBrackets(op2), ",", &endPtr)) << 16;
             offset = stringToInt(endPtr);
         } else {
             p = 0 << 24;
-            char* endPtr;
-            rn = stringToInt(removeBrackets(strtok_r(op2, ",", endPtr)));
+            rn = stringToInt(removeBrackets(strtok_r(op2, ",", endPtr))) << 16;
             offset = stringToInt(endPtr);
         }
     }
@@ -65,16 +63,7 @@ int32_t assembleSTR(char* mnemonic, char* op1, char* op2) {
     int32_t rd = stringToInt(op1) << 12;
     int32_t offset;
 
-    if (!strcmp(op2[0], "=")) {
-        if (stringToInt(op2) <= 0xFF) {
-            return assembleDataProcessing("mov", op1, NULL, op2);
-        } else {
-            int32_t expression = stringToInt(op2);
-            //put expression at the end of the program
-            //use the address of this value with the PC as the base register and a
-            //calculated offset (ldr rd,[PC,offset]) where offset = (value address) - [PC]; 
-        }
-    } else if (strlen(op2) <= 5) {
+    if (strlen(op2) <= 5) {
         rn = stringToInt(removeBrackets(op2));
         offset = 0;
     } else {
