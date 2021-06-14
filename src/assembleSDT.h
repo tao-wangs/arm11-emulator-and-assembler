55d25373235ef156814fc1930ff8799ba628a3b9
TaoWang <tw520@imperial.ac.uk>
2021-06-14 17:24:31 +0100

    fix: added array of uint32_t's to function arguments

--- src/assembleSDT.h
+++ src/assembleSDT.h
@@ -6,9 +6,9 @@
 #include "assdataprocessing.h"
 #include "hash.h"
 
-int32_t assembleSDT(char* instruction, int32_t lastAddress, int32_t pc, hashTable *table);
+int32_t assembleSDT(char* instruction, int32_t lastAddress, int32_t pc, hashTable *table, uint32_t values[]);
 
-int32_t assembleLDR(char* mnemonic, char* op1, char* op2, int32_t lastAddress, int32_t pc, hashTable *table);
+int32_t assembleLDR(char* mnemonic, char* op1, char* op2, int32_t lastAddress, int32_t pc, hashTable *table, uint32_t values[]);
 
 int32_t assembleSTR(char* mnemonic, char* op1, char* op2);
 
